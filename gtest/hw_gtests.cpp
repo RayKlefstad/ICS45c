76fb2f1a85dab0920a8c22a1d44b4d3b74d3e280
Mitchel Dickerson <mgdicker@uci.edu>
2023-04-05 22:52:27 -0700

    Added hw_gtests.cpp file.

---
+++ gtest/hw_gtests.cpp
@@ -0,0 +1,22 @@
+// ------------------------- Tests File - test.cpp -------------------------- //
+// This file is for writing your own user tests. Be sure to include your *.h
+// files to be able to access the functions that you wrote for unit testing.
+// An example has been provided, but more documentation is available here:
+// https://github.com/google/googletest/blob/main/docs/primer.md
+// -------------------------------------------------------------------------- //
+
+// Include all of your *.h files you want to unit test:
+
+
+#include <gtest/gtest.h>
+
+namespace {
+
+TEST(Hello, World) {
+  EXPECT_TRUE(true);
+}
+
+// ADD YOUR TESTS HERE:
+
+
+} // anonymous namespace
