159db382e833b66813e1f670b822b190c4df1090
Mitchel Dickerson <mgdicker@uci.edu>
2023-06-12 08:34:34 -0700

    Updated older gtestmain file.

--- gtest/gtestmain.cpp
+++ gtest/gtestmain.cpp
@@ -6,7 +6,35 @@
 
 #include <gtest/gtest.h>
 
-int main(int argc, char **argv) {
+extern "C" {
+
+void __ubsan_get_current_report_data(const char** issue_kind, const char** message,
+                                     const char** filename, unsigned* line, unsigned* col,
+                                     char** memory_addr);
+
+void __ubsan_on_report() {
+  const char* issue_kind;
+  const char* error_message;
+  const char* filename;
+  unsigned line;
+  unsigned col;
+  char* memory_addr;
+
+  __ubsan_get_current_report_data(&issue_kind, &error_message, &filename, &line, &col,
+                                  &memory_addr);
+  FAIL() << "Undefined behavior detected in \"" << filename << ":" << line << ":" << col << '\n'
+         << error_message << '\n';
+}
+
+void asan_report_callback(const char* report) {
+  FAIL() << "Memory error detected:\n" << report << '\n';
+}
+
+void __asan_set_error_report_callback(void (*callback)(const char*));
+}
+
+int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
+  __asan_set_error_report_callback(asan_report_callback);
   return RUN_ALL_TESTS();
 }
