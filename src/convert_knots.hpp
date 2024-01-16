2f4918b61ee34a99d9c15c563540d7e3e2ca00ea
Jad-Doggy <pandajadon@gmail.com>
2024-01-15 18:17:24 -0800

    long boi

--- src/convert_knots.hpp
+++ src/convert_knots.hpp
@@ -3,7 +3,8 @@ using namespace std;
 
 double knots_to_miles_per_minute(int knot) 
 {
+    long long knots = knot;
     double five_tomato = 5280;
-    double doub_knot = ((knot * 6076) / five_tomato) / 60;
+    double doub_knot = ((knots * 6076) / five_tomato) / 60;
     return doub_knot;
 }
